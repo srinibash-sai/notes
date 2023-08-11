1a68622a0c4916f7112c8de5fd132cc0567b4857
Srinivas Sai <srinibash.sai26@gmail.com>
2023-08-12 00:20:34 +0530

    11th commit

--- notes/urls.py
+++ notes/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("",include("main.urls")),
     path('admin/', admin.site.urls),
 ]
-urlpatterns+=staticfiles_urlpatterns()
+# urlpatterns+=staticfiles_urlpatterns()
