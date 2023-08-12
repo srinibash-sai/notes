700b8648ffeae7a91b10925765a997fbef187829
Srinivas Sai <srinibash.sai26@gmail.com>
2023-08-12 16:17:15 +0530

    Admin panel

--- notes/settings.py
+++ notes/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR,'static')
 # ]
 # STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
